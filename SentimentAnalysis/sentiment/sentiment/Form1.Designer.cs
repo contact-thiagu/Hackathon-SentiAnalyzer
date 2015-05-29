a417f34b644aee57464406b5da53f9bdd0ba0f4f
contact-thiagu <contact_thiagu@yahoo.co.in>
2015-05-29 04:06:04 -0700

    CodeRed - SentiAnalyzer
    Roger, Thiagu, Bhuvaneshwari

---
+++ SentimentAnalysis/sentiment/sentiment/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace sentiment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbltotal = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblprctotal = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblfinaltotal = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(143, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate Sentiment";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(329, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(341, 25);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(127, 23);
+            this.buttonBrowse.TabIndex = 2;
+            this.buttonBrowse.Text = "Browse Input file";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Total records avilable for processing";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.lblfinaltotal);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.lblprctotal);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.lbltotal);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(28, 114);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(384, 100);
+            this.panel1.TabIndex = 4;
+            // 
+            // lbltotal
+            // 
+            this.lbltotal.AutoSize = true;
+            this.lbltotal.Location = new System.Drawing.Point(204, 19);
+            this.lbltotal.Name = "lbltotal";
+            this.lbltotal.Size = new System.Drawing.Size(13, 13);
+            this.lbltotal.TabIndex = 4;
+            this.lbltotal.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Processing";
+            // 
+            // lblprctotal
+            // 
+            this.lblprctotal.AutoSize = true;
+            this.lblprctotal.Location = new System.Drawing.Point(84, 44);
+            this.lblprctotal.Name = "lblprctotal";
+            this.lblprctotal.Size = new System.Drawing.Size(13, 13);
+            this.lblprctotal.TabIndex = 6;
+            this.lblprctotal.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(137, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "of";
+            // 
+            // lblfinaltotal
+            // 
+            this.lblfinaltotal.AutoSize = true;
+            this.lblfinaltotal.Location = new System.Drawing.Point(186, 44);
+            this.lblfinaltotal.Name = "lblfinaltotal";
+            this.lblfinaltotal.Size = new System.Drawing.Size(13, 13);
+            this.lblfinaltotal.TabIndex = 8;
+            this.lblfinaltotal.Text = "0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(341, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Browse Output Folder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(9, 56);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(329, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 225);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sentiment Analysis";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbltotal;
+        private System.Windows.Forms.Label lblfinaltotal;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblprctotal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SentimentAnalysis/sentiment/sentiment/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace sentiment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] positiveword;
+        string[] negativeword;
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+            string postivepath = Application.StartupPath + "\\positive-words.txt";
+            string negpath = Application.StartupPath + "\\Negative-words.txt";
+            positiveword = File.ReadAllLines(@postivepath);
+            negativeword = File.ReadAllLines(@negpath);
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int procescnt = 0;
+                string oppath = @textBox2.Text + "\\output" + DateTime.Now.ToString("ddMMMyyyyhhmmss") + ".txt";
+                if (string.IsNullOrEmpty(textBox2.Text))
+                {
+                    MessageBox.Show("Please select the output folder");
+                }
+                if (!string.IsNullOrEmpty(textBox1.Text))
+                {
+                    panel1.Visible = true;
+                    string verbatium = string.Empty; ;
+                    string[] inputrec = File.ReadAllLines(textBox1.Text);
+                    lbltotal.Text = inputrec.Count().ToString();
+                    lblfinaltotal.Text = inputrec.Count().ToString();
+                    foreach (string verbatiums in inputrec)
+                    {
+                        procescnt++;
+                        lblprctotal.Text = procescnt.ToString();
+                        verbatium = verbatiums.ToLower();
+
+                        string[] sentences = Regex.Split(verbatium, @"(?<=[.!?])\s+(?=[A-Z])");
+                        string[] words; string prevchar = string.Empty;
+                        string currentval = string.Empty;
+                        string[] booster = { "horrible", "absolutely", "complete", "completely", "definitely", "especially", "could", "did", "especially", "extremely", "fuckin", "fucking", "hugely", "incredibly", "just", "may", "might", "ought", "overwhelmingly", "really", "should", "slightly", "totally", "very", "would" };
+                        
+                        int positivecnt = 0;
+                        int negativecnt = 0;
+                        int i = 0;
+                        int j = 0;
+                        int k = 0;
+                        foreach (string sentence in sentences)
+                        {                            
+                            words = sentence.Split(' ');
+                            foreach (string word in words)
+                            {                                
+                                if (booster.Contains(word))
+                                {
+
+                                    if (j == 1)
+                                    {
+                                        positivecnt = positivecnt + 2;
+                                    }
+                                    else if (k == 1)
+                                    {
+                                        negativecnt = negativecnt + 2;
+                                    }
+                                    else
+                                    {
+                                        i = 1;
+                                    }
+                                   
+                                }
+                                else if (positiveword.Contains(word))
+                                {                                    
+                                    positivecnt++;
+                                    if (i == 1)
+                                    {
+                                        positivecnt = positivecnt + 2;
+                                    }
+                                    j = 1;
+
+                                }
+                                else if (negativeword.Contains(word))
+                                {                                    
+                                    negativecnt++;
+                                    if (i == 1)
+                                    {
+                                        negativecnt = negativecnt + 2;
+                                    }
+                                    k = 1;
+                                }
+                                else
+                                {
+                                    i = 0;
+                                    j = 0;
+                                    k = 0;                                    
+                                }
+                            }                           
+
+                        }
+                        
+                        int sentiscore = positivecnt - negativecnt;
+                        string sentivalue = "Negative";
+                        if (sentiscore == 0)
+                        {
+                            sentivalue = "Netural";
+                        }
+                        else if (sentiscore > 0)
+                        {
+                            sentivalue = "Positive";
+                        }
+                        writeoutput(sentivalue, oppath);                        
+                    }
+
+                    label2.Text = "Processed ";
+                    label3.Text = "Output available at " + oppath;
+                }
+                else
+                {
+                    panel1.Visible = false;
+                    MessageBox.Show("Please select the input file");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error occured while processing please try again");
+            }
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.DefaultExt = "txt";
+            openFileDialog1.Title = "Browse Text Files";
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+            openFileDialog1.RestoreDirectory = true;            
+            openFileDialog1.InitialDirectory = @"C:\";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            } 
+
+        }
+
+
+        private void writeoutput(string value,string path)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(path, true))
+                {
+                    writer.WriteLine(value);
+                    writer.Flush();                    
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowNewFolderButton = true;
+            folderBrowserDialog1.SelectedPath = @"C:\";
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox2.Text = folderBrowserDialog1.SelectedPath;             
+
+            } 
+        }
+
+
+     
+    }
+}
